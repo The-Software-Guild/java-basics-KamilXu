464bc8f30cad3f8e0494cb871fff9251ab152642
Kamil Xu <kamilxu2016@gmail.com>
2021-09-30 13:32:18 +0100

    000
    test

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/*
+    Testing Code
+*/
+public class Hello {
+
+    public static void main(String[] args){
+
+        for(int i=0;i<10;i++){
+            System.out.println("Testing 1 ... 2 ... 3");
+        }
+    }
+}
